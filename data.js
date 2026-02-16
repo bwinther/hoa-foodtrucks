f5247575c7a96b17b23475ae357471f92b02928f
Bryan Winther <wintherclaw@mac-mini.lan>
2026-02-15 21:23:05 -0700

    Add food trucks from Jun-Jul 2025 emails

--- data.js
+++ data.js
@@ -2,42 +2,86 @@
 // Leesa updates this when Bryan forwards emails
 
 const foodTrucks = [
-    // 2026
-    {
-        date: "2026-02-22",
-        name: "Taco Express",
-        time: "5:00 PM - 8:00 PM",
-        location: "Community Center",
-        description: "Authentic street tacos, burritos, and quesadillas",
-        website: "https://example.com/tacoexpress",
-        menu: "https://example.com/tacoexpress/menu"
-    },
-    {
-        date: "2026-02-15",
-        name: "Pizza Wagon",
-        time: "5:00 PM - 8:00 PM",
-        location: "Community Center",
-        description: "Wood-fired artisan pizzas",
-        website: "https://example.com/pizzawagon",
-        menu: null
-    },
-    {
-        date: "2026-02-08",
-        name: "BBQ Pit Masters",
-        time: "4:00 PM - 7:30 PM",
-        location: "Community Center",
-        description: "Slow-smoked brisket, ribs, and pulled pork",
-        website: "https://example.com/bbqpit",
-        menu: "https://example.com/bbqpit/menu"
-    },
     // 2025 Archive
     {
         date: "2025-08-27",
         name: "Food Truck Event",
         time: "TBD",
-        location: "Westcliff Community",
+        location: "Westcliff Park",
         description: "Final Food Trucks of the Season. Fire & Police departments on site.",
         website: null,
+        menu: null
+    },
+    {
+        date: "2025-07-09",
+        name: "Uptown & Humboldt",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/uptown-humboldt",
+        menu: null
+    },
+    {
+        date: "2025-07-09",
+        name: "Mama Kabob",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/mama-kabob",
+        menu: null
+    },
+    {
+        date: "2025-07-09",
+        name: "BaNomANom",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/banomanom",
+        menu: null
+    },
+    {
+        date: "2025-07-02",
+        name: "Charminar Express",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/charminar-express",
+        menu: null
+    },
+    {
+        date: "2025-07-02",
+        name: "Big Belly Brothers BBQ",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/big-belly-brothers-bbq",
+        menu: null
+    },
+    {
+        date: "2025-07-02",
+        name: "Sweet Cow Moomobile",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/sweet-cow-moomobile",
+        menu: null
+    },
+    {
+        date: "2025-06-25",
+        name: "The Rolling Italian",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/the-rolling-italian",
+        menu: null
+    },
+    {
+        date: "2025-06-25",
+        name: "Wafflerita",
+        time: "4:30 PM - 8:00 PM",
+        location: "Westcliff Park",
+        description: null,
+        website: "https://truckster.com/vendors/wafflerita",
         menu: null
     }
 ];
